e2c91a80c1e193254dce0b1e65f0fd16a27013c3
Ahasan Habib <habibsumoncse@gmail.com>
2020-10-29 10:55:12 +0600

    clear unnecessary code

--- src/main/java/com/spring/crud/controller/EmployeeController.java
+++ src/main/java/com/spring/crud/controller/EmployeeController.java
@@ -62,26 +62,4 @@ public class EmployeeController {
 		return "redirect:/";
 	}
 	
-	
-	@GetMapping("/page/{pageNo}")
-	public String findPaginated(@PathVariable (value = "pageNo") int pageNo, 
-			@RequestParam("sortField") String sortField,
-			@RequestParam("sortDir") String sortDir,
-			Model model) {
-//		int pageSize = 5;
-//		
-////		Page<Employee> page = employeeService.findPaginated(pageNo, pageSize, sortField, sortDir);
-//		List<Employee> listEmployees = page.getContent();
-//		
-//		model.addAttribute("currentPage", pageNo);
-//		model.addAttribute("totalPages", page.getTotalPages());
-//		model.addAttribute("totalItems", page.getTotalElements());
-//		
-//		model.addAttribute("sortField", sortField);
-//		model.addAttribute("sortDir", sortDir);
-//		model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
-//		
-//		model.addAttribute("listEmployees", employeeService.getAllEmployees());
-		return "index";
-	}
 }
