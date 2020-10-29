ff9d0e3a2e9dac0e750c982364b741573c7d56d1
Ahasan Habib <habibsumoncse@gmail.com>
2020-10-29 14:13:28 +0600

    remove unnecessary code

--- src/main/java/com/spring/crud/controller/EmployeeController.java
+++ src/main/java/com/spring/crud/controller/EmployeeController.java
@@ -1,16 +1,12 @@
 package com.spring.crud.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.spring.crud.model.entity.Employee;
@@ -24,37 +20,39 @@ public class EmployeeController {
 
 	// display list of employees
 	@GetMapping("/")
-	public ModelAndView viewHomePage(Model model) {
+	public ModelAndView viewHomePage() {
 		ModelAndView modelAndView = new ModelAndView();
-		model.addAttribute("listEmployees", employeeService.getAllEmployees());
+		modelAndView.addObject("listEmployees", employeeService.getAllEmployees());
 		modelAndView.setViewName("index");
 		return modelAndView;
 	}
 
+	// create model attribute to bind form data
 	@GetMapping("/showNewEmployeeForm")
-	public String showNewEmployeeForm(Model model) {
-		// create model attribute to bind form data
-		Employee employee = new Employee();
-		model.addAttribute("employee", employee);
-		return "new_employee";
+	public ModelAndView showNewEmployeeForm() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("employee", new Employee());
+		modelAndView.setViewName("new_employee");
+		return modelAndView;
 	}
 
+	// save employee to database
 	@PostMapping("/saveEmployee")
 	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
-		// save employee to database
 		employeeService.saveEmployee(employee);
 		return "redirect:/";
 	}
 
 	@GetMapping("/showFormForUpdate/{id}")
-	public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
-
+	public ModelAndView showFormForUpdate(@PathVariable(value = "id") long id) {
+		ModelAndView modelAndView = new ModelAndView();
 		// get employee from the service
 		Employee employee = employeeService.getEmployeeById(id);
 
 		// set employee as a model attribute to pre-populate the form
-		model.addAttribute("employee", employee);
-		return "update_employee";
+		modelAndView.addObject("employee", employee);
+		modelAndView.setViewName("update_employee");
+		return modelAndView;
 	}
 
 	@GetMapping("/deleteEmployee/{id}")
